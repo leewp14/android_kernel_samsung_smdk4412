121a1b05e936b9bb0eb48759e249fbcec8dd3493
bauner <bauner@gmx.de>
2015-07-07 17:49:00 +0200

    power: 100 mA charge current increase
    https://github.com/libcg/raw_kernel/commit/d212a46f87eace809492b4fda14b7aa8c386ad11

--- drivers/power/sec_battery_u1.c
+++ drivers/power/sec_battery_u1.c
@@ -1483,7 +1483,7 @@ static int sec_bat_enable_charging_main(struct sec_bat_info *info, bool enable)
 		switch (info->cable_type) {
 		case CABLE_TYPE_USB:
 			val_type.intval = POWER_SUPPLY_STATUS_CHARGING;
-			val_chg_current.intval = 450;	/* mA */
+			val_chg_current.intval = 550;	/* mA */
 			break;
 		case CABLE_TYPE_AC:
 			val_type.intval = POWER_SUPPLY_STATUS_CHARGING;
@@ -1491,7 +1491,7 @@ static int sec_bat_enable_charging_main(struct sec_bat_info *info, bool enable)
 			break;
 		case CABLE_TYPE_MISC:
 			val_type.intval = POWER_SUPPLY_STATUS_CHARGING;
-			val_chg_current.intval = 450;	/* mA */
+			val_chg_current.intval = 550;	/* mA */
 			break;
 		default:
 			dev_err(info->dev, "%s: Invalid func use\n", __func__);
@@ -1572,7 +1572,7 @@ static int sec_bat_enable_charging_sub(struct sec_bat_info *info, bool enable)
 			switch (info->cable_type) {
 			case CABLE_TYPE_USB:
 				val_type.intval = POWER_SUPPLY_STATUS_CHARGING;
-				val_chg_current.intval = 450;	/* mA */
+				val_chg_current.intval = 550;	/* mA */
 				break;
 			case CABLE_TYPE_AC:
 				val_type.intval = POWER_SUPPLY_STATUS_CHARGING;
@@ -1586,7 +1586,7 @@ static int sec_bat_enable_charging_sub(struct sec_bat_info *info, bool enable)
 				break;
 			case CABLE_TYPE_MISC:
 				val_type.intval = POWER_SUPPLY_STATUS_CHARGING;
-				val_chg_current.intval = 450;	/* mA */
+				val_chg_current.intval = 550;	/* mA */
 				break;
 			default:
 				dev_err(info->dev, "%s: Invalid func use\n",
@@ -2793,7 +2793,7 @@ static ssize_t sec_bat_store(struct device *dev,
 					switch (info->cable_type) {
 					case CABLE_TYPE_USB:
 					case CABLE_TYPE_MISC:
-						value.intval = 450;	/* mA */
+						value.intval = 550;	/* mA */
 						break;
 					case CABLE_TYPE_AC:
 						value.intval = 650;	/* mA */
