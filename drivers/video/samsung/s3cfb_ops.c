ff6e7e4a0d0abd3fbdd516a056d882aadac8d371
leewp14 <lee.wp14@gmail.com>
2020-03-10 19:06:22 +0800

    s3cfb: custom bootlogo
    https://github.com/leewp14/Samsung-logo

--- drivers/video/samsung/s3cfb_ops.c
+++ drivers/video/samsung/s3cfb_ops.c
@@ -53,6 +53,8 @@
 #include <plat/s5p-sysmmu.h>
 #endif
 
+#include "logo_rgb24_cust.h"
+
 #define SUPPORT_LPM_PAN_DISPLAY
 
 #if defined(CONFIG_S6D7AA0_LSL080AL02)
@@ -147,6 +149,8 @@ int s3cfb_draw_logo(struct fb_info *fb)
 		memcpy(fb->screen_base, logo_virt_buf, fb->var.yres * fb->fix.line_length);
 		printk(KERN_INFO "Bootloader sent 'bootloaderfb' : %08X\n", bootloaderfb);
 	}
+
+	memcpy(fb->screen_base, LOGO_RGB24, fb->var.yres * fb->fix.line_length);
 
 #endif /* #ifdef RGB_BOOTSCREEN */
 #endif
