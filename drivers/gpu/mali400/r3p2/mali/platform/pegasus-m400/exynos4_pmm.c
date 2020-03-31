fe71280815a181928fbbbccef290d0b7167e112b
leewp14 <lee.wp14@gmail.com>
2020-03-31 09:55:48 +0800

    mali400: reset to 0ef0f071edf955e4a7562c87cc085b373464d380
    Change-Id: I31af65647e90ab33cbf38e489daab21ad937517b

--- drivers/gpu/mali400/r3p2/mali/platform/pegasus-m400/exynos4_pmm.c
+++ drivers/gpu/mali400/r3p2/mali/platform/pegasus-m400/exynos4_pmm.c
@@ -38,7 +38,7 @@
 #include <linux/workqueue.h>
 
 #ifdef CONFIG_CPU_EXYNOS4210
-#define MALI_DVFS_STEPS 3
+#define MALI_DVFS_STEPS 2
 #define MALI_DVFS_WATING 10 /* msec */
 #define MALI_DVFS_DEFAULT_STEP 0
 #else
@@ -87,11 +87,7 @@ typedef struct mali_runtime_resumeTag{
 	unsigned int step;
 }mali_runtime_resume_table;
 
-#if defined(CONFIG_CPU_EXYNOS4212) || defined(CONFIG_CPU_EXYNOS4412)
 mali_runtime_resume_table mali_runtime_resume = {266, 900000, 1};
-#else
-mali_runtime_resume_table mali_runtime_resume = {160, 950000, 0};
-#endif
 
 /* dvfs table */
 mali_dvfs_table mali_dvfs[MALI_DVFS_STEPS]={
@@ -102,9 +98,8 @@ mali_dvfs_table mali_dvfs[MALI_DVFS_STEPS]={
 			/* step 3 */{440  ,1000000	,1025000   ,85   , 90},
 			/* step 4 */{533  ,1000000	,1075000   ,95   ,100} };
 #else
-			/* step 0 */{160  ,1000000	, 950000   ,0    , 85},
-			/* step 1 */{200  ,1000000	, 975000   ,75   , 90},
-			/* step 2 */{267  ,1000000	,1000000   ,75   ,100} };
+			/* step 0 */{134  ,1000000	, 950000   ,85   , 90},
+			/* step 1 */{267  ,1000000	,1050000   ,85   ,100} };
 #endif
 
 #ifdef EXYNOS4_ASV_ENABLED
@@ -171,22 +166,16 @@ static unsigned int asv_3d_volt_4212_9_table[MALI_DVFS_STEPS][ASV_LEVEL_PD] = {
 #else
 
 static unsigned int asv_3d_volt_4210_12_table[MALI_DVFS_STEPS][ASV_LEVEL_4210_12] = {
-	{  1000000,  1000000,  1000000,   950000,   950000,   950000,   950000,   950000},	/* L2(160Mhz) */
+	{  1000000,  1000000,  1000000,   950000,   950000,   950000,   950000,   950000},	/* L1(134Mhz) */
 #if (MALI_DVFS_STEPS > 1)
-	{  1050000,  1050000,  1050000,   975000,   975000,   975000,   975000,   950000},	/* L1(200Mhz) */
-#if (MALI_DVFS_STEPS > 2)
-	{  1100000,  1100000,  1100000,  1000000,  1000000,  1000000,  1000000,   950000},	/* L0(267Mhz) */
-#endif
+	{  1100000,  1100000,  1100000,  1000000,  1000000,  1000000,  1000000,   950000},	/* L0(266Mhz) */
 #endif
 };
 
 static unsigned int asv_3d_volt_4210_14_table[MALI_DVFS_STEPS][ASV_LEVEL_4210_14] = {
-	{  1000000,  1000000,   950000,   950000,   950000},	/* L2(160Mhz) */
+	{  1000000,  1000000,   950000,   950000,   950000},	/* L1(134Mhz) */
 #if (MALI_DVFS_STEPS > 1)
-	{  1050000,  1050000,   975000,   975000,   950000},	/* L1(200Mhz) */
-#if (MALI_DVFS_STEPS > 2)
-	{  1100000,  1100000,  1000000,  1000000,   950000},	/* L0(267Mhz) */
-#endif
+	{  1100000,  1100000,  1000000,  1000000,   950000},	/* L0(266Mhz) */
 #endif
 };
 #endif
@@ -223,7 +212,7 @@ int mali_gpu_vol = 1025000;
 /* Orion */
 static const mali_bool bis_vpll = MALI_FALSE;
 int mali_gpu_clk = 267;
-int mali_gpu_vol = 1000000;
+int mali_gpu_vol = 1050000;
 #endif
 
 static unsigned int GPU_MHZ	= 1000000;
@@ -586,7 +575,7 @@ static mali_bool set_mali_dvfs_status(u32 step,mali_bool boostup)
 #endif
 
 #ifdef EXYNOS4_ASV_ENABLED
-	if (samsung_rev() < EXYNOS4412_REV_2_0 && !soc_is_exynos4210()) { 
+	if (samsung_rev() < EXYNOS4412_REV_2_0) {
 		if (mali_dvfs[step].clock == 160)
 			exynos4x12_set_abb_member(ABB_G3D, ABB_MODE_100V);
 		else
@@ -971,7 +960,7 @@ static mali_bool init_mali_clock(void)
 	mali_regulator_set_voltage(mali_gpu_vol, mali_gpu_vol);
 
 #ifdef EXYNOS4_ASV_ENABLED
-	if (samsung_rev() < EXYNOS4412_REV_2_0 && !soc_is_exynos4210()) { 
+	if (samsung_rev() < EXYNOS4412_REV_2_0) {
 		if (mali_gpu_clk == 160)
 			exynos4x12_set_abb_member(ABB_G3D, ABB_MODE_100V);
 		else
@@ -1040,7 +1029,7 @@ static _mali_osk_errcode_t enable_mali_clocks(void)
 		mali_regulator_set_voltage(mali_runtime_resume.vol, mali_runtime_resume.vol);
 
 #ifdef EXYNOS4_ASV_ENABLED
-	if (samsung_rev() < EXYNOS4412_REV_2_0 && !soc_is_exynos4210()) { 
+		if (samsung_rev() < EXYNOS4412_REV_2_0) {
 			if (mali_runtime_resume.clk == 160)
 				exynos4x12_set_abb_member(ABB_G3D, ABB_MODE_100V);
 			else
