734863f50f89d4bec67b32372a232c29c8721f9e
leewp14 <lee.wp14@gmail.com>
2020-04-06 10:46:48 +0800

    Revert "charge_current: adjust charging current"
    Many users might be using an old battery or non-quality battery and a high charging current will decrease the battery runtime.
    Reverting this for now. Let the users control this.
    
    This reverts commit fa3ece9ec0857aca80a7d75d093c044fadf7216d.
    
    Change-Id: I217116cd4252a079b47a433fa53b9ac61f9eb02c

--- drivers/power/charge_current.c
+++ drivers/power/charge_current.c
@@ -12,12 +12,12 @@
 #include <linux/platform_device.h>
 #include <linux/miscdevice.h>
 
-#define MAX_CURRENT_AC   1000
-#define MAX_CURRENT_MISC 1000
-#define MAX_CURRENT_USB  1000
-int charge_current_ac = 980;
+#define MAX_CURRENT_AC   950
+#define MAX_CURRENT_MISC 950
+#define MAX_CURRENT_USB  950
+int charge_current_ac = 650;
 int charge_current_misc = 450;
-int charge_current_usb = 980;
+int charge_current_usb = 450;
 
 
 static ssize_t charge_current_show(struct device *dev, struct device_attribute *attr, char *buf) {
